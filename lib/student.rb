c2c53936eae8e1d74a6fbadebf3c6cae3f44e16b
dallas_blackwell <mdvayne923@gmail.com>
2020-06-06 14:31:37 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,10 @@ class Student
   def initialize
     @super = super
   end
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+  def raise_hand
+    puts "Pick me!"
+  end
 end
